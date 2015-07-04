a9ee745dafad5394d50699d97b5f215da376b59b
Kevin Hoyt <parkerkrhoyt@gmail.com>
2015-07-03 21:42:57 -0600

    Started working on a master screen.
    Eventually will be a master/detail flow.  Master will list the entries
    from a Cloudant database.  Detail will show the specific entry.
    Considering an edit screen.

--- pottytime/app/src/main/java/com/ibm/us/pottytime/DetailActivity.java
+++ pottytime/app/src/main/java/com/ibm/us/pottytime/DetailActivity.java
@@ -1,36 +1,23 @@
 package com.ibm.us.pottytime;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
-
-    private Button  btnDetail = null;
+public class DetailActivity extends Activity {
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_master );
-
-        btnDetail = ( Button )findViewById( R.id.button_detail );
-        btnDetail.setOnClickListener( new View.OnClickListener() {
-            public void onClick( View view ) {
-                Log.i( "DETAIL", "Transition to detail." );
-            }
-        } );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
     }
-/*
+
     @Override
-    public boolean onCreateOptionsMenu( Menu menu ) {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.menu_main, menu );
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -48,5 +35,4 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-*/
 }
