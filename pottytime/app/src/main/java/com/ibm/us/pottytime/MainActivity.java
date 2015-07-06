4aa0291b2009bfdfc1b88b1c10cff4eb6f094f44
Kevin Hoyt <parkerkrhoyt@gmail.com>
2015-07-06 14:27:53 -0600

    Added list to master screen.
    Top half of master screen will be a chart of potties over the past
    week.  Bottom half is a historical list.  List navigates to detail
    screen.  Need to figure out what line item should look like.

--- pottytime/app/src/main/java/com/ibm/us/pottytime/MainActivity.java
+++ pottytime/app/src/main/java/com/ibm/us/pottytime/MainActivity.java
@@ -8,53 +8,55 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
-    private Button  btnDetail = null;
+    private Button              btnDetail = null;
+    private ListView            lstHistory = null;
+
+    private ArrayList<Potty>    history = null;
+    private PottyAdapter        adapter = null;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_master );
 
-        btnDetail = ( Button )findViewById( R.id.button_detail );
-        btnDetail.setOnClickListener( new View.OnClickListener() {
-            public void onClick( View view ) {
-                Log.i( "DETAIL", "Transition to detail." );
+        // Data model
+        history = new ArrayList<Potty>();
+        adapter = new PottyAdapter( this, history );
 
-                Intent detail = null;
-
-                detail = new Intent( MainActivity.this, DetailActivity.class );
-                // detail.putExtra( PottyDetailFragment.ARG_ITEM_ID, id );
-                startActivity( detail );
-            }
-        } );
-    }
-/*
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu ) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.menu_main, menu );
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Testing
+        for( int p = 0; p < 100; p++ )
+        {
+            Potty   test = new Potty();
+            history.add( test );
         }
 
-        return super.onOptionsItemSelected(item);
+        // List
+        lstHistory = ( ListView )findViewById( R.id.list_history );
+        lstHistory.setAdapter( adapter );
+        lstHistory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick( AdapterView<?> parent, View view, int position, long id ) {
+                Intent  detail = null;
+                Potty   potty = null;
+
+                potty = ( Potty )parent.getAdapter().getItem( position );
+
+                detail = new Intent( MainActivity.this, DetailActivity.class );
+                // detail.putExtra( EXTRA_MESSAGE, message );
+                startActivity( detail );
+            }
+
+        } );
     }
-*/
 
 }
---
+++ pottytime/app/src/main/java/com/ibm/us/pottytime/Potty.java
@@ -0,0 +1,17 @@
+package com.ibm.us.pottytime;
+
+import java.util.Date;
+
+public class Potty {
+
+    boolean pee = false;
+    boolean poo = false;
+    Date    when = null;
+    float   latitude = 0;
+    float   longitude = 0;
+    float   temperature = 0;
+    int     color = 0;
+    int     rating = 0;
+    String  condition = null;
+
+}
