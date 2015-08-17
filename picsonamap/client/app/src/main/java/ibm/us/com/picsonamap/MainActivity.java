e8962ee9a8d303636978fcf1183404f46be678ac
Kevin Hoyt <parkerkrhoyt@gmail.com>
2015-08-17 12:22:31 -0600

    Moved to Volley over Cloudant Sync.
    Could not get Cloudant sync to function.  Tried various approaches.
    Documentation seems out of sync with current release (classes like
    RequestBuilder would not resolve, or marshalling errors on connection).
     Decided to ditch it for simple REST.

--- picsonamap/client/app/src/main/java/ibm/us/com/picsonamap/MainActivity.java
+++ picsonamap/client/app/src/main/java/ibm/us/com/picsonamap/MainActivity.java
@@ -1,41 +1,31 @@
 package ibm.us.com.picsonamap;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.GridView;
 
-import com.badoo.mobile.util.WeakHandler;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-import com.ibm.mobile.services.cloudcode.IBMCloudCode;
-import com.ibm.mobile.services.core.IBMBluemix;
-import com.ibm.mobile.services.data.IBMData;
-import com.ibm.mobile.services.data.IBMDataObject;
-import com.ibm.mobile.services.data.IBMQuery;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import bolts.Continuation;
-import bolts.Task;
 
 public class MainActivity extends AppCompatActivity
 {
-    public final static String CLASS_IMAGE = "Image";
-    public final static String KEY_ID = "objectId";
-    public final static String KEY_LATITUDE = "latitude";
-    public final static String KEY_LONGITUDE = "longitude";
-    public final static String KEY_SOURCE = "source";
-    public final static String PATH_APPS = "apps";
-    public final static String PATH_PUBLIC = "public";
-    public final static String PATH_STREAM = "stream";
-
-    private IBMCloudCode            ibmcloud = null;
-    private IBMData                 ibmdata = null;
-
-    private WeakHandler             handler = null;
+    private static final String KEY_DOCUMENTS = "docs";
+    private static final String KEY_ID = "_id";
+    private static final String KEY_LATITUDE = "latitude";
+    private static final String KEY_LONGITUDE = "longitude";
+    private static final String SERVICE_ATTACHMENT = "/attachment/";
+    private static final String SERVICE_PICTURE = "/picture";
+    private static final String SERVICE_ROOT = "https://pics-on-a-map.mybluemix.net/api";
 
     private ArrayList<StreamItem>   stream = null;
     private GridView                grid = null;
@@ -43,86 +33,71 @@ public class MainActivity extends AppCompatActivity
 
     protected void loadStream()
     {
-        IBMQuery<IBMDataObject> query;
+        JsonObjectRequest   request;
 
-        // Query images
-        query = IBMQuery.queryForClass( CLASS_IMAGE );
+        request = new JsonObjectRequest(
+            Request.Method.GET,
+            SERVICE_ROOT + SERVICE_PICTURE,
+            null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse( JSONObject response ) {
 
-        query.find().continueWith( new Continuation<List<IBMDataObject>, Void>() {
+                    JSONArray   docs = null;
+                    JSONObject  record;
+                    StreamItem  item;
+                    String      source = null;
 
-            @Override
-            public Void then( Task<List<IBMDataObject>> task) throws Exception {
-                Bundle              bundle;
-                List<IBMDataObject> results;
-                Message             message;
-                String              source;
+                    try {
+                        docs = response.getJSONArray( KEY_DOCUMENTS );
+                    } catch( JSONException jsone ) {
+                        Log.d( "VOLLEY", jsone.getMessage() );
+                    }
 
-                if( task.isFaulted() )
-                {
-                    Log.i( "MOBILEDATA", "Problem querying image stream." );
-                } else {
-                    // Get the results
-                    results = task.getResult();
-                    Log.i( "MOBILEDATA", "Found " + results.size() + " images in the stream." );
-
-                    // Construct messages
-                    // Send from data access thread to UI thread
-                    for( int i = 0; i < results.size(); i++ )
+                    // Iterate latest images
+                    // Add to dataset
+                    for( int d = 0; d < docs.length(); d++ )
                     {
-                        // Source
-                        source =
-                            getResources().getString( R.string.bluemix_route ) + "/" +
-                            getResources().getString( R.string.bluemix_name ) + "/" +
-                            getResources().getString( R.string.bluemix_version ) + "/" +
-                            PATH_APPS + "/" +
-                            getResources().getString( R.string.bluemix_id ) + "/" +
-                            PATH_PUBLIC + "/" +
-                            PATH_STREAM + "/" +
-                            results.get( i ).getObject( KEY_SOURCE );
+                        try {
+                            record = docs.getJSONObject( d );
+
+                            source = SERVICE_ROOT + SERVICE_ATTACHMENT + record.getString( KEY_ID );
+
+                            // Populate item object
+                            item = new StreamItem();
+                            item.setId( record.getString( KEY_ID ) );
+                            item.setSource( source );
+                            item.setLatitude( record.getDouble( KEY_LATITUDE ) );
+                            item.setLongitude( record.getDouble( KEY_LONGITUDE ) );
+
+                            stream.add( item );
+                        } catch( JSONException jsone ) {
+                            Log.d( "VOLLEY", jsone.getMessage() );
+                        }
 
                         // Debug
-                        Log.i( "MOBILEDATA", source );
-
-                        // Build data to send
-                        bundle = new Bundle();
-                        bundle.putString( KEY_SOURCE, source );
-                        bundle.putString( KEY_ID, results.get( i ).getObjectId() );
-                        bundle.putDouble( KEY_LATITUDE, ( Double )results.get( i ).getObject( KEY_LATITUDE ) );
-                        bundle.putDouble( KEY_LONGITUDE, ( Double )results.get( i ).getObject( KEY_LONGITUDE ) );
-
-                        // Message wrapper for data
-                        message = new Message();
-                        message.setData( bundle );
-
-                        // Send message to UI thread
-                        handler.sendMessage( message );
+                        Log.i( "VOLLEY", source );
                     }
-                }
 
-                return null;
+                    // Refresh the view
+                    adapter.notifyDataSetChanged();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse( VolleyError error ) {
+                    Log.d( "VOLLEY", error.getMessage() );
+                }
             }
-        } );
+        );
+
+        Volley.newRequestQueue( this ).add( request );
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState )
+    protected void onCreate( Bundle savedInstanceState )
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Initialize Bluemix
-        IBMBluemix.initialize(
-            this,
-            getResources().getString( R.string.bluemix_id ),
-            getResources().getString( R.string.bluemix_secret ),
-            getResources().getString( R.string.bluemix_route )
-        );
-
-        // Initialize Cloud Code
-        ibmcloud = IBMCloudCode.initializeService();
-
-        // Initialize Mobile Data
-        ibmdata = IBMData.initializeService();
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
         // Image grid data model
         stream = new ArrayList<>();
@@ -131,38 +106,6 @@ public class MainActivity extends AppCompatActivity
         // Assign data model to component
         grid = ( GridView )findViewById( R.id.grid_stream );
         grid.setAdapter( adapter );
-
-        // Weak handler implementation
-        // https://techblog.badoo.com/blog/2014/08/28/android-handler-memory-leaks
-        handler = new WeakHandler( new Handler.Callback() {
-
-            @Override
-            public boolean handleMessage( Message message ) {
-                Bundle      bundle;
-                StreamItem  item;
-
-                // Message data
-                bundle = message.getData();
-
-                // Populate item object
-                item = new StreamItem();
-                item.setId( bundle.getString( KEY_ID ) );
-                item.setSource(bundle.getString(KEY_SOURCE));
-                item.setLatitude(bundle.getDouble(KEY_LATITUDE));
-                item.setLongitude( bundle.getDouble( KEY_LONGITUDE ) );
-
-                Log.i( "MOBILEDATA", bundle.getString( KEY_ID ) );
-
-                // Push into listing
-                stream.add( item );
-
-                // Tell adapter to update
-                adapter.notifyDataSetChanged();
-
-                return false;
-            }
-
-        } );
 
         // Load recent stream
         loadStream();
