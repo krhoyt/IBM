c8e923469d914bdb1a8d225a5fa0b6efb7fc0f25
Kevin Hoyt <parkerkrhoyt@gmail.com>
2015-08-28 15:56:08 -0600

    Started implementing a finished product.
    Added some design, gauges, switch, colors.  Having problems with
    calculating tick mark labels.

--- iot/photonlab/utilities/app.js
+++ iot/photonlab/utilities/app.js
@@ -1,13 +1,15 @@
+// Packages
 var express = require( 'express' );
 var path = require( 'path' );
 
+// Express
 var app = express();
 
+// Static routing
+// To "web" directory in lab folder
 app.use( express.static( path.join( __dirname, '..', 'web' ) ) );
 
+// Start server
 var server = app.listen( 3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log( 'Listening at: http://%s:%s', host, port );
+  console.log( 'Ready.' );
 } );
