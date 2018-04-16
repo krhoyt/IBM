4816f93bd60623a99548e2b5148f9b55c6885ac2
Kevin Hoyt <parkerkrhoyt@gmail.com>
2018-04-16 12:42:40 -0600

    Visual recognition of my garage.
    For a talk on computer vision vs. visual recognition.

---
+++ watson/garage/watson/training/status.sh
@@ -0,0 +1,2 @@
+## Classifier Details
+curl "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/garage_801373840?api_key=159d1965dc0d7456e33ab7a13a6764e5746989a8&version=2016-05-20"
